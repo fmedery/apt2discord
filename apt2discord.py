9a2d7af67107e79a993cb282bc25275c56b0b33a
Frederic Medery <fmedery@gmail.com>
2024-04-04 10:41:52 -0400

    update git ignore

--- apt2discord.py
+++ apt2discord.py
@@ -1,33 +1,36 @@
-import requests, subprocess, os
-import dotenv
+import requests, subprocess, os, dotenv
 
 dotenv.load_dotenv()
 
 webhook_url = os.getenv('WEBHOOK')
+fqdn=os.uname()[1]
+
+# def update_package_list():
+#     """Updates the package lists for available packages."""
+#     command = "sudo apt update -qqq >/dev/null 2>&1"
+#     os.system(command)
 
 def update_package_list():
     """Updates the package lists for available packages."""
-    command = "sudo apt update -qqq"
-    os.system(command)
+    output = subprocess.check_output(["apt", "update", "-qqq"], text=True)
+
 
 def check_for_updates():
     """Checks for available Debian package updates and returns a list of those with new versions."""
     output = subprocess.check_output(["apt", "list", "--upgradable"], text=True)
-    # output = subprocess.check_output(["apt", "search","writer2latex"], text=True)
     updates = []
     for line in output.splitlines():
-        if line.startswith("write"):
-            updates.append(line.split()[1])
+        if line.startswith(line):
+            updates.append(line)
+            # updates.append(line.split()[0])
     return updates
 
 def send_update_message(update_list):
   """Sends a message containing the list of updates to the Discord channel."""
-  message = "**Available Updates:**\n" + "\n".join(update_list)
+  message = "Available Updates: on " "**" + fqdn + "**" + "\n" + "\n".join(update_list)
   data = {"content": message}
   response = requests.post(webhook_url, json=data)
-  if response.status_code == 204:
-      print("Update message sent to Discord successfully!")
-  else:
+  if not response.status_code == 204:
       print(f"Error sending message: {response.status_code}")
 
 
