444f8308c0f492e7d78951b4f7dd6a4b81e6564f
Frederic Medery <fmedery@gmail.com>
2024-04-05 19:18:18 -0400

    update code

--- apt2discord.py
+++ apt2discord.py
@@ -1,16 +1,13 @@
 import requests, subprocess, os, dotenv, pathlib, sys
 
-webhook_url = os.getenv('WEBHOOK')
-fqdn=os.uname()[1]
-
 def update_package_list():
     """Updates the package lists for available packages."""
-    output = subprocess.check_output(["apt", "update", "-qqq"], text=True)
+    output = subprocess.check_output(["apt", "update"], stderr=subprocess.DEVNULL, text=True) # Capture as text
 
 
 def check_for_updates():
     """Checks for available Debian package updates and returns a list of those with new versions."""
-    output = subprocess.check_output(["apt", "list", "--upgradable"], text=True)
+    output = subprocess.check_output(["apt", "list", "--upgradable"], stderr=subprocess.DEVNULL, text=True)
     updates = []
     for line in output.splitlines():
         if line.startswith(line):
@@ -19,25 +16,27 @@ def check_for_updates():
     return updates
 
 def send_update_message(update_list):
-  """Sends a message containing the list of updates to the Discord channel."""
-  message = "Available Updates: on " "**" + fqdn + "**" + "\n" + "\n".join(update_list)
-  data = {"content": message}
-  response = requests.post(webhook_url, json=data)
-  if not response.status_code == 204:
-      print(f"Error sending message: {response.status_code}")
+    fqdn=os.uname()[1]
+    """Sends a message containing the list of updates to the Discord channel."""
+    message = "Available Updates: on " "**" + fqdn + "**" + "\n" + "\n".join(update_list)
+    data = {"content": message}
+    response = requests.post(webhook_url, json=data)
+    if not response.status_code == 204:
+        print(f"Error sending message: {response.status_code}")
 
 
 if __name__ == "__main__":
     
     if len(sys.argv) < 2:  # Check for at least one argument
-        print("Error: Please provide the envfile location. \n")
+        print("Error: Please provide the conf_file location. \n")
         print("Usage:")
-        print(os.path.basename(sys.argv[0]) + " envfile_location")
+        print(os.path.basename(sys.argv[0]) + " conf_file_location")
         sys.exit(1)  # Exit with an error code (optional)
     else:
         dotenv_path = pathlib.Path(sys.argv[1])
         dotenv.load_dotenv(dotenv_path)
-
+        webhook_url = os.getenv('WEBHOOK')
+    
     update_package_list()  # Update package lists first
 
     available_updates = check_for_updates()
